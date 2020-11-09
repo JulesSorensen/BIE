0762ee6c4fbd4b3737318391fb4ca583b1532fa2
Nekewo <julessorens@gmail.com>
2020-11-09 16:16:48 +0100

    maj

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ client.on('message', msg => {
     // || msg.author.id === "336458121180610560"
     if (msg.author.id === client.user.id) return;
 
-    if(msg.content.toLocaleLowerCase() === "ping") {
-        msg.reply("pong!");
-    }
-
     if (msg.content.toLocaleLowerCase().startsWith("bonjour") || msg.content.toLocaleLowerCase().startsWith("salut")) {
         msg.channel.send(`Bonjour ${msg.author} !`);
     }
@@ -27,28 +23,37 @@ client.on('message', msg => {
     if (msg.content.toLowerCase().startsWith("!repeat")) {
         msg.delete({ timeout: 100 })
         const arg = msg.content.slice(1).split(' ');
-        ann = "";
+        ann = ""
         for (let i = 1; i < arg.length; i++) {
             if (i === 1) { ann = ann + arg[i] } else { ann = ann + " " + arg[i] }
         }
-        msg.channel.send(ann);
+        msg.channel.send(ann)
     }
-
-    if (msg.content.toLocaleLowerCase() === "<@!765648471058546698>") {
-        msg.channel.send("Arrête de me ping <@" + msg.author.id + "> !");
-    }
-
-    if (msg.content.toLocaleLowerCase() === "BIE") {msg.channel.send("Quelqu'un m'a appelé ?");}
-
-    if (msg.content.toLocaleLowerCase().startsWith("!mp")) {
+    if (msg.content.toLowerCase().startsWith("!spam")) {
+        msg.delete({ timeout: 100 })
         const arg = msg.content.slice(1).split(' ');
-        mpmessage = "";
-        for (let i = 2; i < arg.length; i++) {
-            if (i === 1) { mpmessage = mpmessage + arg[i] } else { mpmessage = mpmessage + " " + arg[i] }
+        ann = ""
+        for (let i = 1; i < arg.length; i++) {
+            if (i === 1) { ann = ann + arg[i] } else { ann = ann + " " + arg[i] }
         }
-        let authoruser = client.users.cache.get(arg[1]);
-        authoruser.send("Une personne annonyme t'a envoyé \"" + mpmessage + " \"");
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
+        msg.channel.send(ann)
     }
+
+    if (msg.content.toLocaleLowerCase() === "<@765648471058546698>") {
+        msg.channel.send("Arrête de me ping <@" + msg.author.id + "> !")
+    }
+
+    if (msg.content.toLocaleLowerCase() === "BIE") msg.channel.send("Quelqu'un m'a appelé ?")
+
 });
 
 client.login(process.env.TOKEN);
