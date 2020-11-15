6cf91cd45a0b47f3ed9aca5d6c41102880fd9ecc
Nekewo <julessorens@gmail.com>
2020-11-15 18:08:37 +0100

    V1.2.0

--- index.js
+++ index.js
@@ -1,58 +1,284 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { get } = require("snekfetch"); 
+const fs = require('fs');
+let prefix = "$"
+let version = "1.2.0";
+let efficomstud = ["427408019114950667", "160804942565736449", "220571668458766337", "394929882049675264", "697717795227697173", "231827158878781441", "400318649191104522", "105457483740368896", "765135022985707542", "228599908939202560", "233248965032804353", "608379884548653068", "763108903201538069", "448796874183540736", "304366314850353154", "762699664184967240", "475986569455599616", "345681524386955265", "235723505604362240", "336458121180610560"];
+let swimagefichier = fs.readFileSync("./sw.txt").toString();
+let swimages = swimagefichier.split("\n");
+let childimagefichier = fs.readFileSync("./child.txt").toString();
+let childimages = childimagefichier.split("\n");
+var lancement = false;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.username}!`);
     client.user.setPresence({
         activity: {
-            name: 'Jules',
-            type: "WATCHING",
-        },
-        status: "dnd"
+            name: '$help',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/efficom_tech_school"
+        }
     });
 });
 
+// emojis
+function emoji(id) {
+    return client.emojis.get(id).toString();
+}
+
 client.on('message', msg => {
+    // function couleuraleatoire() {
+    //     if (msg.guild.id == "762698485011054602");
+    //     setInterval(function(){
+    //         var coleurrandom = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+    //         let colorrole = msg.guild.roles.cache.get('775777327669444619');
+    //         colorrole.edit({ color : coleurrandom }, { reason: 'Bot' }).catch(error => client.catch(error));
+    //     }, 1000)
+    // }
+    // if (lancement == false) {
+    //     lancement = true;
+    //     couleuraleatoire();
+    // }
     // || msg.author.id === "336458121180610560"
     if (msg.author.id === client.user.id) return;
+    // bonjour
+    BJ = ["bonjour", "salut", "hei ", "guttentag", "hallo ", "hola ", "buenos días", "hey", "yo ", "yop ", "coucou"];
+    for (let i = 0; i < BJ.length; i++) {
+        if (msg.content.toLowerCase().includes(BJ[i])) {
+            msg.react("👋")
+        };
+    };
+    // mdr lol ...
+    LOL = ["mdr", "lol", "xd", "x)", "😂", "🤣", "😆"]
+    for (let i2 = 0; i2 < LOL.length; i2++) {
+        if (msg.content.toLowerCase().includes(LOL[i2])) {
+            hasard = Math.floor(Math.random() * 3) + 1;
+            if (hasard === 1) {
+                msg.react("😂");
+            } else if (hasard === 2) {
+                msg.react("🤣");
+            } else {
+                msg.react("😆");
+            };
+        };
+    };
 
-    if (msg.content.toLocaleLowerCase().startsWith("bonjour") || msg.content.toLocaleLowerCase().startsWith("salut")) {
-        msg.channel.send(`Bonjour ${msg.author} !`);
+    // annonce jules
+    if (msg.content.toLowerCase().startsWith("$a")) {
+        if (msg.author.id != "697717795227697173") return;
+        const arg = msg.content.slice(prefix.length).split(' ');
+        ann = ""
+        for (let i = 1; i < arg.length; i++) {
+            if (i == 1) { ann = ann + arg[i] } else { ann = ann + " " + arg[i] }
+        }
+        let efficomsalon = client.channels.cache.get("776093234005934111");
+        efficomsalon.send(ann);   
+    }
+    // teams
+    if (msg.content.toLowerCase().startsWith("$teams")) {
+        msg.delete({timeout:10});
+        const arg = msg.content.slice(prefix.length).split(' ');
+        ann = "";
+        for (let i = 1; i < arg.length; i++) {
+            if (i == 1) { ann = ann + arg[i] } else { ann = ann + " " + arg[i] }
+        }
+        let efficomsalon = client.channels.cache.get("762698661892849714");
+        efficomsalon.send("Une réunion sur Teams va / vient de commencer : " + ann); 
     }
 
-    if (msg.content.toLowerCase().startsWith("!repeat")) {
-        msg.delete({ timeout: 100 })
+    // jeux
+    // pierre feuille ciseau PFC
+    if(msg.content.toLowerCase().startsWith("$pfc") || msg.content.toLowerCase().startsWith("$pierrefeuilleciseau")) {
+        var finpartie = false;
+        var scorejoueur = 0;
+        var scorebot = 0;
+        // fonctions
+        function pfcrandom() {
+            switch (Math.floor(Math.random() * 3) + 1) {
+                case 1:
+                    return "pierre";
+                    break;
+                case 2:
+                    return "feuille";
+                    break;
+                case 3:
+                    return "ciseau";
+                    break;
+            }
+        };
+        function scoreactuel() {
+            if (scorejoueur == 3 || scorebot == 3) {
+                finpartie = true;
+            };
+            return;
+        }
+        function pfcjeu() {
+            msg.reply("**choississez `pierre` `feuille` ou `ciseau`, entrez votre réponse ci dessous ⤵**")
+            msg.channel.awaitMessages(m => m.author.id == msg.author.id, { max: 1, time: 15000 }).then(collected => {
+                repjoueur = collected.first().content;
+                if (repjoueur.toLowerCase().startsWith("p")) { // si le joueur choisi pierre
+                    pfcbot = pfcrandom();
+                    if (pfcbot == "pierre") { // pierre contre pierre
+                        msg.reply("👊 **contre** 👊, égalité !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    } else if (pfcbot == "feuille") {
+                        scorebot ++;
+                        msg.reply("👊 **contre** ✋, vous perdez !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    } else {
+                        scorejoueur ++;
+                        msg.reply("👊 **contre** 🤞, vous gagnez !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    }
+                } else if (repjoueur.toLowerCase().startsWith("f")) { // si le joueur choisi feuille
+                    pfcbot = pfcrandom()
+                    if (pfcbot == "pierre") { // feuille contre pierre
+                        scorejoueur ++;
+                        msg.reply("✋ **contre** 👊, vous gagnez !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    } else if (pfcbot == "feuille") {
+                        msg.reply("✋ **contre** ✋, égalité !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    } else {
+                        scorebot ++;
+                        msg.reply("✋ **contre** 🤞, vous perdez !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    }
+                } else if (repjoueur.toLowerCase().startsWith("c")) { // si le joueur choisi ciseau
+                    pfcbot = pfcrandom()
+                    if (pfcbot == "pierre") { // ciseau contre pierre
+                        scorebot ++;
+                        msg.reply("🤞 **contre** 👊, vous perdez !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    } else if (pfcbot == "feuille") {
+                        scorejoueur ++;
+                        msg.reply("🤞 **contre** ✋, vous gagnez !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    } else {
+                        msg.reply("🤞 **contre** 🤞, égalité !\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");
+                    }
+                } else {scorebot ++; msg.reply("vous avez répondu n'importe quoi donc vous perdez un point...\n" + "\n__Score__: " + scorejoueur + " / " + scorebot + "\n<@" + msg.author.id + "> / <@" + client.user.id + ">\n­");}
+                scoreactuel();
+                if (finpartie == false) {
+                    pfcjeu();
+                } else {
+                    if (scorejoueur > scorebot) {
+                        return msg.channel.send("🏆 **La partie est finie, __vous avez gagné__ <@" + msg.author.id + ">...**");
+                    } else if (scorejoueur > scorebot) {
+                        return msg.channel.send("😎 **La partie est finie, __vous avez perdu__ <@" + msg.author.id + "> !**");
+                    };
+                }
+            }).catch(() => { msg.reply("vous n'avez pas répondu assez vite, j'annule la partie..."); finpartie = true;});
+        };
+        pfcjeu();
+    }
+
+    // random
+    if(msg.content.toLowerCase().startsWith('$chat') || msg.content.toLowerCase().startsWith('$cat')) {
+		try {
+			get('https://aws.random.cat/meow').then(res => {
+				const embed = new Discord.MessageEmbed()
+				.setImage(res.body.file)
+				return msg.channel.send({embed});
+			});
+		} catch(err) {
+			return msg.channel.send(err.stack);
+		}
+    }
+    if(msg.content.toLowerCase().startsWith("$sw") || msg.content.toLowerCase().startsWith("$starwars")) {
+        let rd = Math.floor(Math.random() * swimages.length);
+        const embed = new Discord.MessageEmbed()
+        .setImage(swimages[rd])
+        return msg.channel.send({embed});
+    }
+    if(msg.content.toLowerCase().startsWith("$by") || msg.content.toLowerCase().startsWith("$child") || msg.content.toLowerCase().startsWith("$babyyoda")) {
+        let rd = Math.floor(Math.random() * childimages.length);
+        const embed = new Discord.MessageEmbed()
+        .setImage(childimages[rd])
+        return msg.channel.send({embed});
+    }
+
+    // ping
+    if (msg.content.startsWith("$ping")) {
+        var ping = Date.now() - msg.createdTimestamp + " ms";
+        msg.reply("le ping a duré `" + `${Date.now() - msg.createdTimestamp}` + " ms` !");
+    }
+
+    if (msg.content.toLowerCase().startsWith("$repeat")) {
+        msg.delete({ timeout: 100 });
         const arg = msg.content.slice(1).split(' ');
-        ann = ""
+        ann = "";
         for (let i = 1; i < arg.length; i++) {
             if (i === 1) { ann = ann + arg[i] } else { ann = ann + " " + arg[i] }
         }
-        msg.channel.send(ann)
-    }
-    if (msg.content.toLowerCase().startsWith("!spam")) {
-        msg.delete({ timeout: 100 })
-        const arg = msg.content.slice(1).split(' ');
-        ann = ""
-        for (let i = 1; i < arg.length; i++) {
-            if (i === 1) { ann = ann + arg[i] } else { ann = ann + " " + arg[i] }
-        }
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
-        msg.channel.send(ann)
+        let salon = client.channels.cache.get('762698661892849714');
+        salon.send("Quelqu'un m'a dit de dire \"" + ann + "\"");
     }
 
-    if (msg.content.toLocaleLowerCase() === "<@765648471058546698>") {
-        msg.channel.send("Arrête de me ping <@" + msg.author.id + "> !")
-    }
+    if (msg.content.toLowerCase().includes("bie")) {msg.channel.send("Quelqu'un m'a appelé ?")};
 
-    if (msg.content.toLocaleLowerCase() === "BIE") msg.channel.send("Quelqu'un m'a appelé ?")
+    // help
+    if (msg.content.toLowerCase().startsWith("$aide") || msg.content.toLowerCase().startsWith("$h")) {
+        const arg = msg.content.slice(prefix.length).split(' ');
+        if (!arg[1]) {
+            msg.channel.send({
+                embed: {
+                    color: 47804,
+                    thumbnail: {
+                        url: "https://www.efficom.fr/wp-content/themes/efficom-lille/img/presentation_ecole_04_.jpeg"
+                    },
+                    author: {
+                        name: "Commande d'aide",
+                        icon_url: msg.author.avatarURL()
+                    },
+                    title: "Voilà quelques commandes avec leurs significations !\n­",
+                    description: "BIE est un robot développé par Jules L en 2020.\n­",
+                    fields: [
+                    {
+                        name: "`$pfc` / `$pierrefeuilleciseau`",
+                        value: "Lance une partie de pierre feuille ciseau contre moi ! Vous pouvez répondre `p` `f` ou `c` à la place de pierre feuille ou ciseau... C'est plus rapide !\n­"
+                    }, {
+                        name: "`$teams [link]`",
+                        value: "Mets un message dans le salon <#762698661892849714> pour prévenir d'une réunion sur Teams qui commence. Mettez dans la variable `link` le lien de cette réunion.\n­"
+                    }, {
+                        name: "`$help image` / `$h i` / `$help i`",
+                        value: "Il y a plusieurs commandes pour générer des images. Grâce à ça vous pourrez avoir leur liste.\n­"
+                    }, {
+                        name: "`$repeat [text]`",
+                        value: "Répète le texte entré dans la variable `text`. Le message sera obligatoirement renvoyé dans <#762698661892849714>.\n­"
+                    }, {
+                        name: "`bonjour`, `salut` / `😂`, `😆` etc...",
+                        value: "BIE est un robot qui réagit ! Il vous répondera évidemment à sa façon ! :)\n­"
+                    }, {
+                        name: "`$ping`",
+                        value: "Pour avoir le temps de réponse de votre message avec sa réception sur Discord.\n­"
+                    }],
+                    timestamp: new Date(),
+                    footer: {
+                        text: "BIE Version " + version + " | Demande d'aide réclamé par " + msg.author.tag
+                    }
+                }
+            })
+        } else if (arg[1].toLowerCase().startsWith("i")) {
+            msg.channel.send({
+                embed: {
+                    color: 47804,
+                    author: {
+                        name: "Liste des commandes d'images",
+                        icon_url: msg.author.avatarURL()
+                    },
+                    fields: [
+                    {
+                        name: "`$sw` / `$StarWars`",
+                        value: "Génère des images aléatoires de l'univers Star Wars.\n­"
+                    }, {
+                        name: "`$by` / `$BabyYoda` / `$child`",
+                        value: "Génère une image aléatoire du surnommé \"Bébé Yoda\".\n­"
+                    }, {
+                        name: "`$chat` / `$cat`",
+                        value: "Il y a plusieurs commandes pour générer des images. Grâce à ça vous pourrez avoir leur liste.\n­"
+                    }],
+                    timestamp: new Date(),
+                    footer: {
+                        text: "BIE Version " + version + " | Demande d'aide réclamé par " + msg.author.tag
+                    }
+                }
+            })
+        } else return msg.reply("je ne reconnais pas cette commande... 😔")
+    }
 
 });
 
