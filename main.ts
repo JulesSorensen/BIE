8bfbf279706a55eb9183b097cc0593b01bb420b0
Jules Sorensen <julesladnef@gmail.com>
2022-01-27 18:31:19 +0100

    replit fix

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import Discord from 'discord.js';
 require('dotenv').config()
+// @ts-ignore
 import { Client, Collection, Intents } from 'discord.js';
 const client = new Client({
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
@@ -11,14 +12,16 @@ const client = new Client({
 });
 // @ts-ignore
 client.commands = new Discord.Collection();
+// @ts-ignore
 import fs from 'file-system'
-
+// @ts-ignore
 import config from './src/config/config.json';
 import { commandLaunch, interactionLaunch } from './src/commands/commandLauch';
 import { reactionAdded, reactionRemoved } from './src/tasks/reactions';
 import { edtReminderCheck, edtSenderCheck, mygesCheck } from './src/tasks/intervalsCheck';
 
 const version = "3.0.0";
+// @ts-ignore
 let prefix = config.prefix;
 
 const authServers = ["762698485011054602", "783679631101526056", "831823187213680682"];
@@ -26,13 +29,13 @@ const bannedUsers = ["858320039286800404", "839602909942906891"];
 const compatibleFolders = ["efficom"];
 
 
-fs.readdir(`./src/commands/`, (error, folders) => {
-    if (error) { return console.log(`Error while trying to get the commmands. 1`, error); };
-    folders.forEach(folder => {
+fs.readdir(`./src/commands/`, (error: any, folders: any) => {
+    if (!!error) { return console.log(`Error while trying to get the commmands. 1`, error); };
+    folders.forEach((folder: any) => {
         if (fs.statSync(`./src/commands/${folder}`).isDirectory() && (compatibleFolders.includes(folder))) {
-            fs.readdir(`./src/commands/${folder}`, (error, files) => {
+            fs.readdir(`./src/commands/${folder}`, (error: any, files: any) => {
                 if (error) { return console.log(`Error while trying to get the commmands. 2`); };
-                files.forEach(file => {
+                files.forEach((file: any) => {
                     const command = require(`./src/commands/${folder}/${file}`);
                     // @ts-ignore
                     client.commands.set(command.name, command);
@@ -44,7 +47,9 @@ fs.readdir(`./src/commands/`, (error, folders) => {
 
 
 client.on('ready', () => {
+    // @ts-ignore
     console.log(`-----\nLogged in as ${client.user.username} !\nVersion: ` + version + ` ✅\n-----\n`);
+    // @ts-ignore
     client.user.setActivity({ name: '&help', type: `WATCHING` });
 
     edtReminderCheck(client);
@@ -54,6 +59,7 @@ client.on('ready', () => {
 
 
 client.on('messageCreate', msg => {
+    // @ts-ignore
     if (authServers.includes(msg.guild.id) && !bannedUsers.includes(msg.author.id)) {
         commandLaunch(msg, config, client, version);
     }
@@ -72,4 +78,4 @@ client.on('messageReactionRemove', (reaction, user) => {
 })
 
 // login
-client.login(process.env.TOKEN);
+client.login(process.env['token']);
