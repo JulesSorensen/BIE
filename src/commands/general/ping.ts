40e7480e72ea34a0bd041f50c78573d2efa7650c
Jules Sorensen <julesladnef@gmail.com>
2022-01-27 16:08:08 +0100

    fix

--- src/commands/general/ping.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'ping',
-	guildOnly: false,
-	execute(msg, args, client, prefix, version) {
-		let time = Date.now() - msg.createdTimestamp;
-        msg.channel.send(`:green_circle: <@${msg.author.id}> ping: ${time}ms`);
-	}
-};
--- src/commands/general/version.ts
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'version',
-	guildOnly: false,
-	execute(msg, args, client, prefix, getca, version) {
-        msg.reply(`current build: v${version}`);
-	}
-};
